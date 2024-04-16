3c6cc873c7f7b7daf7a925a661c16c6e4ac7b617
JavoAlcota <panchoo99full@gmail.com>
2024-04-16 19:36:37 -0400

    Primer commit. Avanzado hasta interfaz de playlists de spotify

---
+++ app/src/main/java/com/udaproject/myapplication/views/SpotifyPlaylistsActivity.kt
@@ -0,0 +1,21 @@
+package com.udaproject.myapplication.views
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.udaproject.myapplication.R
+
+class SpotifyPlaylistsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_spotify_playlists)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
